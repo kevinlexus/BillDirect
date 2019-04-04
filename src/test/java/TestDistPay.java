44eb52f2ea208c44e58bc1643ea0989d3a98f877
LeoLexx <factorkevin@gmail.com>
2019-04-04 07:44:17 +0300

    Тесты МПРО-2 -ver1.2

--- src/test/java/TestDistPay.java
+++ src/test/java/TestDistPay.java
@@ -1,7 +1,8 @@
 import com.dic.app.Config;
+import com.dic.bill.dao.SaldoUslDAO;
+import com.dic.bill.dto.SumUslOrgRec;
 import com.dic.bill.mm.TestDataBuilder;
 import com.dic.bill.model.scott.*;
-import com.ric.cmn.Utl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.math.BigDecimal;
-import java.util.Date;
+import java.util.List;
 
 /**
  * Тесты распределения оплаты
@@ -33,6 +34,8 @@ public class TestDistPay {
 
     @Autowired
     private TestDataBuilder testDataBuilder;
+    @Autowired
+    private SaldoUslDAO saldoUslDao;
 
     @PersistenceContext
     private EntityManager em;
@@ -58,26 +61,27 @@ public class TestDistPay {
         // построить лицевые счета по помещению
         Ko ko = testDataBuilder.buildKartForTest(house, "0001", BigDecimal.valueOf(76.2),
                 3, true, true, 1, 1);
-        Kart kart = em.find(Kart.class, "ОСН_0001");
+        String lsk = "ОСН_0001";
+        Kart kart = em.find(Kart.class, lsk);
 
         // Добавить сальдо
         // прошлый период
-        testDataBuilder.addSaldoUslForTest(kart, 7, "003", "201403", "100.50");
-        testDataBuilder.addSaldoUslForTest(kart, 8, "004", "201403", "100.50");
-        testDataBuilder.addSaldoUslForTest(kart, 9, "004", "201403", "-200.50");
-        testDataBuilder.addSaldoUslForTest(kart, 1, "004", "201403", "-100.50");
+        testDataBuilder.buildSaldoUslForTest(kart, 7, "003", "201403", "10.62");
+        testDataBuilder.buildSaldoUslForTest(kart, 8, "004", "201403", "552.17");
+        testDataBuilder.buildSaldoUslForTest(kart, 9, "004", "201403", "-211.88");
+        testDataBuilder.buildSaldoUslForTest(kart, 1, "004", "201403", "-14.25");
 
         // текущий период
-        testDataBuilder.addSaldoUslForTest(kart, 7, "003", "201404", "200.50");
-        testDataBuilder.addSaldoUslForTest(kart, 4, "005", "201404", "22.53");
-        testDataBuilder.addSaldoUslForTest(kart, 12, "004", "201404", "0.11");
-        testDataBuilder.addSaldoUslForTest(kart, 8, "006", "201404", "10.34");
-        testDataBuilder.addSaldoUslForTest(kart, 4, "004", "201404", "3.79");
-        testDataBuilder.addSaldoUslForTest(kart, 3, "007", "201404", "4.18");
-        testDataBuilder.addSaldoUslForTest(kart, 1, "019", "201404", "-50.19");
-        testDataBuilder.addSaldoUslForTest(kart, 11, "014", "201404", "-100.79");
-        testDataBuilder.addSaldoUslForTest(kart, 13, "031", "201404", "-1");
-        testDataBuilder.addSaldoUslForTest(kart, 9, "015", "201404", "-8.38");
+        testDataBuilder.buildSaldoUslForTest(kart, 7, "003", "201404", "200.50");
+        testDataBuilder.buildSaldoUslForTest(kart, 4, "005", "201404", "22.53");
+        testDataBuilder.buildSaldoUslForTest(kart, 12, "004", "201404", "0.11");
+        testDataBuilder.buildSaldoUslForTest(kart, 8, "006", "201404", "10.34");
+        testDataBuilder.buildSaldoUslForTest(kart, 4, "004", "201404", "3.79");
+        testDataBuilder.buildSaldoUslForTest(kart, 3, "007", "201404", "4.18");
+        testDataBuilder.buildSaldoUslForTest(kart, 1, "019", "201404", "-50.19");
+        testDataBuilder.buildSaldoUslForTest(kart, 11, "014", "201404", "-100.79");
+        testDataBuilder.buildSaldoUslForTest(kart, 10, "031", "201404", "-1");
+        testDataBuilder.buildSaldoUslForTest(kart, 9, "015", "201404", "-8.38");
 
         // Добавить начисление
         testDataBuilder.addChargeForTest(kart, "011", "8.10");
@@ -90,26 +94,26 @@ public class TestDistPay {
         testDataBuilder.addChargeForTest(kart, "015", "0.70");
 
         // Добавить перерасчеты
-        Date dtek = Utl.getDateFromStr("01.04.2014");
-        ChangeDoc changeDoc = new ChangeDoc();
-        changeDoc.setDt(dtek);
+        String strDt = "01.04.2014";
+        String dopl = "201401";
+        ChangeDoc changeDoc = testDataBuilder.buildChangeDocForTest(strDt, dopl);
         testDataBuilder.addChangeForTest(kart, changeDoc, 4, 4, "011",
-                "201404", null, 1, dtek, "118.10");
+                "201404", null, 1, strDt, "118.10");
         testDataBuilder.addChangeForTest(kart, changeDoc, 4, 5, "012",
-                "201404", null, 1, dtek, "7.11");
+                "201404", null, 1, strDt, "7.11");
         testDataBuilder.addChangeForTest(kart, changeDoc, 4, 12, "013",
-                "201404", null, 1, dtek, "3.15");
+                "201404", null, 1, strDt, "3.15");
 
         // Добавить платеж
-        Kwtp kwtp = testDataBuilder.buildKwtpForTest(kart, "201401", "10.04.2014", null, 0,
-                "021", "12313", "01", "100.25", null);
-        KwtpMg kwtpMg = testDataBuilder.addKwtpMgForTest(kwtp, "201401", "20.05", "5.12");
+        Kwtp kwtp = testDataBuilder.buildKwtpForTest(kart, dopl, "10.04.2014", null, 0,
+                "021", "12313", "001", "100.25", null);
+        KwtpMg kwtpMg = testDataBuilder.addKwtpMgForTest(kwtp, dopl, "20.05", "5.12");
         testDataBuilder.addKwtpDayForTest(kwtpMg, 1, "003", 1, "10.05");
         testDataBuilder.addKwtpDayForTest(kwtpMg, 1, "011", 5, "10.00");
         testDataBuilder.addKwtpDayForTest(kwtpMg, 1, "015", 3, "5.00");
         testDataBuilder.addKwtpDayForTest(kwtpMg, 1, "003", 4, "0.12");
 
-        kwtpMg = testDataBuilder.addKwtpMgForTest(kwtp, "201401", "75.08", "0.00");
+        kwtpMg = testDataBuilder.addKwtpMgForTest(kwtp, dopl, "75.08", "0.00");
         testDataBuilder.addKwtpDayForTest(kwtpMg, 1, "003", 1, "50.30");
         testDataBuilder.addKwtpDayForTest(kwtpMg, 1, "011", 6, "19.70");
         testDataBuilder.addKwtpDayForTest(kwtpMg, 1, "011", 4, "5.08");
@@ -122,6 +126,10 @@ public class TestDistPay {
         BigDecimal itgChng = kart.getChange().stream()
                 .map(Change::getSumma).reduce(BigDecimal.ZERO, BigDecimal::add);
         log.info("Итого перерасчеты:{}", itgChng);
+
+        List<SumUslOrgRec> lstSal = saldoUslDao.getSaldoUslByLsk(lsk, "201403");
+        BigDecimal itgSal = lstSal.stream().map(SumUslOrgRec::getSumma).reduce(BigDecimal.ZERO, BigDecimal::add);
+        log.info("Итого сальдо:{}", itgSal);
 
         BigDecimal itgPay = kart.getKwtpDay().stream()
                 .map(KwtpDay::getSumma).reduce(BigDecimal.ZERO, BigDecimal::add);
