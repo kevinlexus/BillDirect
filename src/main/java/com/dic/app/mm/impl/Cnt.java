111e12ded0ac546fb3c50effdeacbf386f88f3d5
LeoLexx <factorkevin@gmail.com>
2019-10-18 16:27:22 +0300

    Исправления класса Cnt

--- src/main/java/com/dic/app/mm/impl/Cnt.java
+++ src/main/java/com/dic/app/mm/impl/Cnt.java
@@ -9,8 +9,10 @@ import java.util.List;
  * подсчет
  */
 public class Cnt {
-	public long cntSal = 0L;
-	public long cntDeb = 0L;
-	public List<SumDebUslMgRec> lstSalNd;
-	public List<SumDebMgRec> lstDebNd;
+
+	long cntSal = 0L;
+	long cntDeb = 0L;
+	List<SumDebUslMgRec> lstSalNd;
+	List<SumDebMgRec> lstDebNd;
+	
 }
