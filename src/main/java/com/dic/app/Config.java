5f9aeecde6a3d37fc2485092254d330aa3487ae7
tln <factorkevin@gmail.com>
2020-05-13 12:39:04 +0300

    проверка git-10

--- src/main/java/com/dic/app/Config.java
+++ src/main/java/com/dic/app/Config.java
@@ -40,9 +40,9 @@ public class Config  implements ApplicationContextAware, AsyncConfigurer {
 		ctx = context;
 	}
 
-	// проверка нового комментария 2
-	// Добавил новый коммментарий 2
-	// Еще одну строку 2
+	// проверка нового комментария 3
+	// Добавил новый коммментарий 3
+	// Еще одну строку 3
 	@Bean
 	public CacheManager cacheManager() {
 		SimpleCacheManager cacheManager = new SimpleCacheManager();
